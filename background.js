420353d1ee5aeca3a4c0e993a4c78b50e663f4c5
Jay Lee <jay0lee@gmail.com>
2018-09-30 13:41:46 -0400

    First code upload

---
+++ background.js
@@ -0,0 +1,84 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+  if ( details.reason === 'installed' ) {
+    console.log('detected extension installed, running data cleanup.');
+    cleanData();
+  } else {
+    console.log('ignoring onInstalled reason ' + details.reason);
+  }
+});
+
+chrome.storage.onChanged.addListener(function (changes, areaName) {
+  console.log('detected new app policy, running data cleanup.');
+  cleanData();
+});
+
+function cleanData() {
+  chrome.storage.managed.get(null, function(storage_items) {
+    console.log('managed settings: ');
+    console.log(storage_items);
+    if ( storage_items.clear_general_data ) {
+      var RemovalTypes = {
+        "since": parseInt(storage_items.general_data_remove_newer_than, 10),
+        "originTypes": { "unprotectedWeb": true }
+      };
+      var DataTypeSet = {};
+      for (var myindex in storage_items.general_data_to_clear) {
+        element = storage_items.general_data_to_clear[myindex];
+        DataTypeSet[element] = true;
+      }
+      console.log('RemovalTypes: ');
+      console.log(RemovalTypes);
+      console.log('DataTypeSet: ');
+      console.log(DataTypeSet);
+      chrome.browsingData.remove(RemovalTypes, DataTypeSet, function() {
+        console.log("cleared general data!");
+      });
+    } else {
+      console.log('general data clearing disabled');
+    };
+    if ( storage_items.clear_cookies ) {
+      for (var mycp in storage_items.cookie_patterns_to_clear) {
+        var pattern = storage_items.cookie_patterns_to_clear[mycp];
+        console.log('pattern:');
+        console.log(pattern);
+        chrome.cookies.getAll(pattern, function (cookies) {
+          for (myck in cookies) {
+            var cookie = cookies[myck];
+            var url = '';
+            url += cookie.secure ? 'https://' : 'http://';
+            url += cookie.domain.charAt(0) == '.' ? 'www' : '';
+            url += cookie.domain;
+            url += cookie.path;
+            var details = {
+                            "url": url,
+                            "name": cookie.name,
+                            "storeId": cookie.storieId
+                          }
+            console.log('deleting cookie: ');
+            console.log(details);
+            chrome.cookies.remove(details, function (dets) {
+              console.log('removed cookie ' + dets.name + ' for ' + dets.url);
+            });
+          };
+        });
+      };
+    } else {
+      console.log('clearing cookies disabled');
+    };
+    if ( storage_items.clear_history ) {
+      for ( var myhist in storage_items.history_searches_to_clear ) {
+        search = storage_items.history_searches_to_clear[myhist];
+        console.log('searching history for ' + search);
+        chrome.history.search({"text": search}, function (historyItems) {
+          for ( var myit in historyItems ) {
+            historyItem = historyItems[myit];
+            console.log('removing history url ' + historyItem.url);
+            chrome.history.deleteUrl({"url": historyItem.url});
+          };
+        });
+      };
+    } else {
+      console.log('clearing history disabled');
+    };
+  });
+}
---
+++ update-cws.sh
@@ -0,0 +1,89 @@
+# Store our credentials in our home directory with a file called .
+my_creds=~/.`basename $0`
+
+appid=$1
+zipfilepath=$2
+zipfile=$appid.zip
+
+rm $zipfile
+zip -r $zipfile $zipfilepath
+
+# https://developers.google.com/identity/protocols/OAuth2InstalledApp#creatingcred
+client_id='311118430818-9btaroouc454mgp3pe4e5d60t5q0ok61.apps.googleusercontent.com'
+client_secret='wYKfNtR_3lc3f1n74Nv69lrk'
+
+if [ -s $my_creds ]; then
+  # if we already have a token stored, use it
+  . $my_creds
+  time_now=`date +%s`
+else
+  scope='https://www.googleapis.com/auth/chromewebstore'
+  # Form the request URL
+  # https://developers.google.com/identity/protocols/OAuth2InstalledApp#step-2-send-a-request-to-googles-oauth-20-server
+  auth_url="https://accounts.google.com/o/oauth2/v2/auth?client_id=$client_id&scope=$scope&response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
+
+  echo "Please go to:"
+  echo
+  echo "$auth_url"
+  echo
+  echo "after accepting, enter the code you are given:"
+  read auth_code
+
+  # exchange authorization code for access and refresh tokens
+  # https://developers.google.com/identity/protocols/OAuth2InstalledApp#exchange-authorization-code
+  auth_result=$(curl -s "https://www.googleapis.com/oauth2/v4/token" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d code=$auth_code \
+    -d client_id=$client_id \
+    -d client_secret=$client_secret \
+    -d redirect_uri=urn:ietf:wg:oauth:2.0:oob \
+    -d grant_type=authorization_code)
+  access_token=$(echo -e "$auth_result" | \
+                 grep -Po '"access_token" *: *.*?[^\\]",' | \
+                 awk -F'"' '{ print $4 }')
+  refresh_token=$(echo -e "$auth_result" | \
+                  grep -Po '"refresh_token" *: *.*?[^\\]",*' | \
+                  awk -F'"' '{ print $4 }')
+  expires_in=$(echo -e "$auth_result" | \
+               grep -Po '"expires_in" *: *.*' | \
+               awk -F' ' '{ print $3 }' | awk -F',' '{ print $1}')
+  time_now=`date +%s`
+  expires_at=$((time_now + expires_in - 60))
+  echo -e "access_token=$access_token\nrefresh_token=$refresh_token\nexpires_at=$expires_at" > $my_creds
+fi
+
+# if our access token is expired, use the refresh token to get a new one
+# https://developers.google.com/identity/protocols/OAuth2InstalledApp#offline
+if [ $time_now -gt $expires_at ]; then
+  refresh_result=$(curl -s "https://www.googleapis.com/oauth2/v4/token" \
+   -H "Content-Type: application/x-www-form-urlencoded" \
+   -d refresh_token=$refresh_token \
+   -d client_id=$client_id \
+   -d client_secret=$client_secret \
+   -d grant_type=refresh_token)
+  access_token=$(echo -e "$refresh_result" | \
+                 grep -Po '"access_token" *: *.*?[^\\]",' | \
+                 awk -F'"' '{ print $4 }')
+  expires_in=$(echo -e "$refresh_result" | \
+               grep -Po '"expires_in" *: *.*' | \
+               awk -F' ' '{ print $3 }' | awk -F',' '{ print $1 }')
+  time_now=`date +%s`
+  expires_at=$(($time_now + $expires_in - 60))
+  echo -e "access_token=$access_token\nrefresh_token=$refresh_token\nexpires_at=$expires_at" > $my_creds
+fi
+
+# call the CWS API to upload draft
+# https://developer.chrome.com/webstore/using_webstore_api#uploadexisitng
+  api_data=$(curl -s -X PUT https://www.googleapis.com/upload/chromewebstore/v1.1/items/$appid \
+    -H "Authorization: Bearer $access_token" \
+    -H "x-goog-api-version: 2" \
+    -H "Content-Type: application/json" \
+    -T $zipfile)
+  echo -e "$api_data"
+
+# publish draft
+  publish_data=$(curl -s -X POST https://www.googleapis.com/chromewebstore/v1.1/items/$appid/publish \
+    -H "Authorization: Bearer $access_token"  \
+    -H "x-goog-api-version: 2" \
+    -H "Content-Length: 0")
+  echo -e "$publish_data"
